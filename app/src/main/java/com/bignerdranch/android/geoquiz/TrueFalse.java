299f0bec7cf9e48bf1c46c10e318bde3e9c43863
john-mckillip <john0mckillip@gmail.com>
2015-01-15 12:23:17 -0600

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/TrueFalse.java
@@ -0,0 +1,31 @@
+package com.bignerdranch.android.geoquiz;
+
+/**
+ * Created by johnmckillip on 1/14/15.
+ */
+public class TrueFalse {
+
+    private int mQuestion;
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion) {
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+    }
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(int question) {
+        mQuestion = question;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean trueQuestion) {
+        mTrueQuestion = trueQuestion;
+    }
+}
